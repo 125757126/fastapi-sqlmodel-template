2470c8cfc9e3adadfa9a06164d58e0ae6198f9bb
125757126 <125757126@users.noreply.github.com>
2021-10-04 05:21:17 +0000

    ✅ Ready to clone and code.

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -1,6 +1,6 @@
 # How to develop on this project
 
-project_name welcomes contributions from the community.
+fastapi_sqlmodel_template welcomes contributions from the community.
 
 **You need PYTHON3!**
 
@@ -8,9 +8,9 @@ This instructions are for linux base systems. (Linux, MacOS, BSD, etc.)
 ## Setting up your own fork of this repo.
 
 - On github interface click on `Fork` button.
-- Clone your fork of this repo. `git clone git@github.com:YOUR_GIT_USERNAME/project_urlname.git`
-- Enter the directory `cd project_urlname`
-- Add upstream repo `git remote add upstream https://github.com/author_name/project_urlname`
+- Clone your fork of this repo. `git clone git@github.com:YOUR_GIT_USERNAME/fastapi-sqlmodel-template.git`
+- Enter the directory `cd fastapi-sqlmodel-template`
+- Add upstream repo `git remote add upstream https://github.com/125757126/fastapi-sqlmodel-template`
 
 ## Setting up your own virtual environment
 
--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-"""Python setup.py for project_name package"""
+"""Python setup.py for fastapi_sqlmodel_template package"""
 import io
 import os
 from setuptools import find_packages, setup
@@ -6,7 +6,7 @@ from setuptools import find_packages, setup
 
 def read(*paths, **kwargs):
     """Read the contents of a text file safely.
-    >>> read("project_name", "VERSION")
+    >>> read("fastapi_sqlmodel_template", "VERSION")
     '0.1.0'
     >>> read("README.md")
     ...
@@ -30,17 +30,17 @@ def read_requirements(path):
 
 
 setup(
-    name="project_name",
-    version=read("project_name", "VERSION"),
-    description="project_description",
-    url="https://github.com/author_name/project_urlname/",
+    name="fastapi_sqlmodel_template",
+    version=read("fastapi_sqlmodel_template", "VERSION"),
+    description="Awesome fastapi_sqlmodel_template created by 125757126",
+    url="https://github.com/125757126/fastapi-sqlmodel-template/",
     long_description=read("README.md"),
     long_description_content_type="text/markdown",
-    author="author_name",
+    author="125757126",
     packages=find_packages(exclude=["tests", ".github"]),
     install_requires=read_requirements("requirements.txt"),
     entry_points={
-        "console_scripts": ["project_name = project_name.__main__:main"]
+        "console_scripts": ["fastapi_sqlmodel_template = fastapi_sqlmodel_template.__main__:main"]
     },
     extras_require={"test": read_requirements("requirements-test.txt")},
 )
